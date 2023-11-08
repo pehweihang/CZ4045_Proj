4e44d9fe306c74f7770d0a80e4c6e935a1579224
weihang <pehweihang@live.com>
2023-11-08 18:09:40 +0800

    set slurm output folder

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@
 #SBATCH --nodes=1
 #SBATCH --mem=10G
 #SBATCH --job-name=NLP
-#SBATCH --output=output_%x_%j.out
-#SBATCH --error=error_%x_%j.err
+#SBATCH --output=slurm_out/output_%x_%j.out
+#SBATCH --error=slurm_out/error_%x_%j.err
 
 module load anaconda
 source activate nlp-proj
